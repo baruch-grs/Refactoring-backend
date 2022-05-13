e8ccec267616a30a21f1c410a15ff3bf3290c291
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-12 22:09:48 -0500

    Added linter to the project and fixed the linter problems

--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -1,28 +1,28 @@
-const FB = require('../../lib/services/FizzBuzzService');
+const FB = require("../../lib/services/FizzBuzzService");
 
-describe('Suite Test for FizzBuzzService class', () => {
-  test('Case 1. Tests for applyValidationInExplorer method', () => {
-    const explorer = { name: 'explorer1', score: 1 };
-    const result = FB.applyValidationInExplorer(explorer);
-    expect(result).toBeDefined();
-    expect(result.trick).toBe(1);
-  });
-  test('Case 2. Test for applyValidationInExplorer method, second use case assigning FIZZ word to trick', () => {
-    const explorer = { name: 'explorer2', score: 3 };
-    const result = FB.applyValidationInExplorer(explorer);
-    expect(result).toBeDefined();
-    expect(result.trick).toBe('FIZZ');
-  });
-  test('Case 3. Test for applyValidationInExplorer method, third use case assigning BUZZ word to trick', () => {
-    const explorer = { name: 'julio', score: 5 };
-    const result = FB.applyValidationInExplorer(explorer);
-    expect(result).toBeDefined();
-    expect(result.trick).toBe('BUZZ');
-  });
-  test('Case 4. Test for applyValidationInExplorer method, fourth use case when is divisible by 3 and 5', () => {
-    const explorer = { name: 'juan', score: 15 };
-    const result = FB.applyValidationInExplorer(explorer);
-    expect(result).toBeDefined();
-    expect(result.trick).toBe('FIZZBUZZ');
-  });
+describe("Suite Test for FizzBuzzService class", () => {
+    test("Case 1. Tests for applyValidationInExplorer method", () => {
+        const explorer = { name: "explorer1", score: 1 };
+        const result = FB.applyValidationInExplorer(explorer);
+        expect(result).toBeDefined();
+        expect(result.trick).toBe(1);
+    });
+    test("Case 2. Test for applyValidationInExplorer method, second use case assigning FIZZ word to trick", () => {
+        const explorer = { name: "explorer2", score: 3 };
+        const result = FB.applyValidationInExplorer(explorer);
+        expect(result).toBeDefined();
+        expect(result.trick).toBe("FIZZ");
+    });
+    test("Case 3. Test for applyValidationInExplorer method, third use case assigning BUZZ word to trick", () => {
+        const explorer = { name: "julio", score: 5 };
+        const result = FB.applyValidationInExplorer(explorer);
+        expect(result).toBeDefined();
+        expect(result.trick).toBe("BUZZ");
+    });
+    test("Case 4. Test for applyValidationInExplorer method, fourth use case when is divisible by 3 and 5", () => {
+        const explorer = { name: "juan", score: 15 };
+        const result = FB.applyValidationInExplorer(explorer);
+        expect(result).toBeDefined();
+        expect(result.trick).toBe("FIZZBUZZ");
+    });
 });
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,10 +1,10 @@
-const Reader = require('../../lib/utils/Reader');
-const path = require('path');
+const Reader = require("../../lib/utils/Reader");
+const path = require("path");
 
-const myPath = path.join(__dirname, '../dummy.json');
-describe('Suite test for class Reader', () => {
-  test('Case 1. Test for readjsonFile method', () => {
-    const data = Reader.readJsonFile(myPath);
-    expect(data).toBeDefined();
-  });
+const myPath = path.join(__dirname, "../dummy.json");
+describe("Suite test for class Reader", () => {
+    test("Case 1. Test for readjsonFile method", () => {
+        const data = Reader.readJsonFile(myPath);
+        expect(data).toBeDefined();
+    });
 }); 
